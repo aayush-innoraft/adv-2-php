97655ae957e5f7afeaacaf45a71db63e0ed32256
aayush-innoraft <aayush.pathak@innoraft.com>
2025-04-09 11:55:01 +0530

    added mail

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include("./backend.php")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>User Input Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<!-- form container  -->
+    <div class="form-container">
+        <!-- h2 in form container  -->
+        <h2>User Info</h2>
+        <!-- php to show if the message is send -->
+         <!-- or an error occured   -->
+        <?php if ($message): ?>
+            <p style="color: green;"><?= $message ?></p>
+        <?php endif; ?>
+        <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" placeholder="Your name" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Your email" required>
+            </div>
+
+            <button type="submit" name="btn">Submit</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
