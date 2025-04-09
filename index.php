7131a3b96b63b648ebc22e799410044c74bcf7a3
aayush-innoraft <aayush.pathak@innoraft.com>
2025-04-09 12:03:34 +0530

    added-

--- index.php
+++ index.php
@@ -13,30 +13,32 @@ include("./backend.php")
 </head>
 
 <body>
-<!-- form container  -->
+    <!-- form container  -->
     <div class="form-container">
         <!-- h2 in form container  -->
         <h2>User Info</h2>
         <!-- php to show if the message is send -->
-         <!-- or an error occured   -->
+        <!-- or an error occured   -->
         <?php if ($message): ?>
             <p style="color: green;"><?= $message ?></p>
         <?php endif; ?>
+        <!-- form  -->
         <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+            <!-- username input div  -->
             <div class="form-group">
                 <label for="name">Name:</label>
                 <input type="text" id="name" name="name" placeholder="Your name" required>
             </div>
 
             <div class="form-group">
+                <!-- email input div  -->
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email" placeholder="Your email" required>
             </div>
-
+            <!-- button to submit form  -->
             <button type="submit" name="btn">Submit</button>
         </form>
     </div>
-
 </body>
 
 </html>
